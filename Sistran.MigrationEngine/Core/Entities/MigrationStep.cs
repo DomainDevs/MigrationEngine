91c72600420ccf95fcdab6444cc961814673a88e
Fernando <dario.fernando.poveda@gmail.com>
2026-03-05 08:24:52 -0500

    Protección Core

---
+++ Sistran.MigrationEngine/Core/Entities/MigrationStep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class MigrationStep
+    {
+        public string NombrePaso { get; set; }
+        public bool Exito { get; set; }
+        public string Mensaje { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
+    }
+}
