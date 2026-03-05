bbc8e9b1a7e1c17d979cf6054dddb0d315f4d1aa
Fernando <dario.fernando.poveda@gmail.com>
2026-03-05 13:32:12 -0500

    Actualización configuración

---
+++ Sistran.MigrationEngine/MigrationExecutor/Utils/MigrationConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace MigrationExecutor.Utils;
+
+public class MigrationConfig
+{
+    public string NombreJob { get; set; } = string.Empty;
+    public string CarpetaPaquetes { get; set; } = string.Empty;
+    public string CarpetaLogs { get; set; } = string.Empty;
+}
