ada180359e19b82996454eb4e53f7df3c4b559f6
Fernando <dario.fernando.poveda@gmail.com>
2026-03-05 21:48:49 -0500

    Swagger

--- Sistran.MigrationEngine/Executors/MigrationExecutor.WebAPI/Utils/Startup.cs
+++ Sistran.MigrationEngine/Executors/MigrationExecutor.WebAPI/Utils/Startup.cs
@@ -20,14 +20,12 @@ public static class Startup
     }
     public static void Configure(WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MigrationExecutor API v1");
-            });
-        }
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "MigrationExecutor API v1");
+        });
 
         app.UseHttpsRedirection();
         app.UseAuthorization();
