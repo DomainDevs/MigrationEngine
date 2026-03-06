f54e1f8c69d3a2a4d2ed49728c5aae9513a49fe6
Fernando <dario.fernando.poveda@gmail.com>
2026-03-05 20:49:22 -0500

    swagger

--- Sistran.MigrationEngine/Executors/MigrationExecutor.WebAPI/Program.cs
+++ Sistran.MigrationEngine/Executors/MigrationExecutor.WebAPI/Program.cs
@@ -1,15 +1,22 @@
-using Infrastructure;
 using Engine;
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MigrationExecutor.WebAPI.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Cargar configuración desde appsettings.json
 builder.Configuration.AddJsonFile("Configurations/appsettings.json", optional: false, reloadOnChange: true);
 builder.Configuration.AddJsonFile("Configurations/documentation.json", optional: false, reloadOnChange: true);
+
+// Para poder inyectar directamente MigrationConfig, opcionalmente:
+builder.Services.AddSingleton(sp =>
+    sp.GetRequiredService<IOptions<MigrationConfig>>().Value
+);
 
 // Registrar servicios de Infrastructure y Engine
 var rutaLogs = builder.Configuration.GetValue<string>("Migration:CarpetaLogs");
